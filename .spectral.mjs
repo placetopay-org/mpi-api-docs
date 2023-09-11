524bdd5727cabefd7eebfe833bae30f920e50782
Omar A. Barbosa Ortiz <contacto@omarbarbosa.com>
2023-09-11 09:17:24 -0500

    Modified 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIxNzc3Ng/spectral.js?branch_slug=master&token=9e536b82-e521-48f1-a8e0-a8d09d1d9f12";
+export default { extends: ruleset };
